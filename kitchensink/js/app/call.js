a1e265b2457cdafc75c02e677fb12277add1682b
Peter Jashchenko <peter@jaybirdgroup.com>
2016-04-06 19:16:45 +0300

    Updated kitchensink call functionality

--- kitchensink/js/app/call.js
+++ kitchensink/js/app/call.js
@@ -35,9 +35,49 @@ define(['jquery', 'ApiLoader'], function($, Ctl) {
                     }
                 );
 
+                speakEasy.CallManager.onCallReceived = function (call) {
+
+                    // Call dialog box
+                    var r = confirm("Incoming call! Would you like to answer?");
+                    if (r === true) {
+                        // Answering on the incoming call
+                        call.answer(function() {
+                            showInfoMessage("Call is answered!");
+                        }, function() {
+                            showErrorMessage("Call couldn't be answered!");
+                        });
+                    } else {
+                        // Rejecting the incomming call
+                        call.reject(function() {
+                            showInfoMessage("Call is rejected!");
+                        }, function() {
+                            showErrorMessage("Call couldn't be rejected!");
+                        });
+                    }
+                };
+
                 btnMakeCall.addEventListener("click", function (e) {
                     var numToCall = confDestination.value;
-                    speakEasy.CallManager.createCall(numToCall, true);
+                    speakEasy.CallManager.createCall(numToCall, false, function(call) {
+
+                        call.onStateChanged = function(state) {
+                            switch (state) {
+
+                                case call.events.CALL_STARTED:
+                                    showInfoMessage("Call is started!");
+                                    break;
+                                case call.events.CALL_ENDED:
+                                    showInfoMessage("Call was ended on other side!");
+                                    break;
+                                case call.events.CALL_HELD:
+                                    showInfoMessage("Call was held on other side!");
+                                    break;
+                                case call.events.CALL_REJECTED:
+                                    showInfoMessage("Call was rejected!");
+                                    break;
+                            }
+                        }
+                    });
                 });
 
                 btnEndCall.addEventListener("click", function (e) {
@@ -73,8 +113,30 @@ define(['jquery', 'ApiLoader'], function($, Ctl) {
                    );
                 });
 
-            }
+                btnHoldCall.addEventListener("click", function (e) {
+                    var currentCall = speakEasy.CallManager.getCurrentCall();
+                    currentCall.hold(
+                        function() {
+                            showInfoMessage("Call is held!");
+                        },
+                        function() {
+                            showErrorMessage("Call couldn't be held!");
+                        }
+                    );
+                });
 
+                btnUnHoldCall.addEventListener("click", function (e) {
+                    var currentCall = speakEasy.CallManager.getCurrentCall();
+                    currentCall.unhold(
+                        function() {
+                            showInfoMessage("Call is resumed!");
+                        },
+                        function() {
+                            showErrorMessage("Call couldn't be resumed!");
+                        }
+                    );
+                });
+            }
         });
     }
 
