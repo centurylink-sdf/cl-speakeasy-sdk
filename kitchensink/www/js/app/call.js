52a6289ff0d7419684bd85a6ae3187b1579cd9d5
Andrew Kovalenko <andrew@jaybirdgroup.com>
2016-06-07 16:52:09 +0300

    fix styling issues

--- kitchensink/www/js/app/call.js
+++ kitchensink/www/js/app/call.js
@@ -43,8 +43,8 @@ define(['jquery', 'CtlApiLoader'], function($, Ctl) {
 
                 speakEasy.CallManager.setup(
                     {
-                        localVideoContainer: "localVideo",
-                        remoteVideoContainer: "remoteVideo"
+                        localVideoContainer: 'localVideo',
+                        remoteVideoContainer: 'remoteVideo'
                     }
                 );
 
@@ -57,29 +57,29 @@ define(['jquery', 'CtlApiLoader'], function($, Ctl) {
                     addCall(call.getCallId(), { name: callerInfo.name, number: callerInfo.number, status: 'Incoming' });
 
                     // Call dialog box
-                    var r = confirm("Incoming call from "+ callerInfo.name +"(" + callerInfo.number + ")! Would you like to answer?");
+                    var r = confirm('Incoming call from '+ callerInfo.name +'(' + callerInfo.number + ')! Would you like to answer?');
                     if (r === true) {
                         // Answering on the incoming call
                         call.answer(function() {
-                            showInfoMessage("Call is answered!");
+                            showInfoMessage('Call is answered!');
                             updateCallButtonsGroup();
                         }, function() {
-                            showErrorMessage("Call couldn't be answered!");
+                            showErrorMessage('Call couldn\'t be answered!');
                         });
                     } else {
                         // Rejecting the incomming call
                         call.reject(function() {
 
                         }, function() {
-                            showErrorMessage("Call couldn't be rejected!");
+                            showErrorMessage('Call couldn\'t be rejected!');
                         });
                     }
                 };
 
-                btnMakeCall.addEventListener("click", function (e) {
+                btnMakeCall.addEventListener('click', function (e) {
                     var numToCall = confDestination.value;
 
-                    $("#btnGroupCall").show();
+                    $('#btnGroupCall').show();
                     confDestination.value = '';
 
                     speakEasy.CallManager.createCall(numToCall, false, function(call) {
@@ -87,11 +87,11 @@ define(['jquery', 'CtlApiLoader'], function($, Ctl) {
                         attachCallListeners(call);
                         addCall(call.getCallId(), { name: '', number: numToCall, status: 'Ringing' });
                     }, function() {
-                        showErrorMessage("Make new call failed!");
+                        showErrorMessage('Make new call failed!');
                     });
                 });
 
-                btnEndCall.addEventListener("click", function (e) {
+                btnEndCall.addEventListener('click', function (e) {
                     var currentCall = speakEasy.CallManager.getCurrentCall();
                     currentCall.hangUp(function() {
 
@@ -102,41 +102,41 @@ define(['jquery', 'CtlApiLoader'], function($, Ctl) {
                             removeCall(currentCall.getCallId());
                         }, 1000);
                     }, function() {
-                        showErrorMessage("Call couldn't be ended!");
+                        showErrorMessage('Call couldn\'t be ended!');
                     });
                 });
 
-                btnStartVideo.addEventListener("click", function (e) {
+                btnStartVideo.addEventListener('click', function (e) {
                     var currentCall = speakEasy.CallManager.getCurrentCall();
                     currentCall.startVideoSend(
                        function() {
-                           showInfoMessage("Video is started!");
-                           $("#localVideoContainer").show();
+                           showInfoMessage('Video is started!');
+                           $('#localVideoContainer').show();
                            $(btnStartVideo).hide();
                            $(btnStopVideo).show();
                        },
                        function() {
-                           showErrorMessage("Video couldn't be started!");
+                           showErrorMessage('Video couldn\'t be started!');
                        }
                    );
                 });
 
-                btnStopVideo.addEventListener("click", function (e) {
+                btnStopVideo.addEventListener('click', function (e) {
                     var currentCall = speakEasy.CallManager.getCurrentCall();
                     currentCall.stopVideoSend(
                        function() {
-                           showInfoMessage("Video is stopped!");
-                           $("#localVideoContainer").hide();
+                           showInfoMessage('Video is stopped!');
+                           $('#localVideoContainer').hide();
                            $(btnStopVideo).hide();
                            $(btnStartVideo).show();
                        },
                        function() {
-                           showErrorMessage("Video couldn't be stopped!");
+                           showErrorMessage('Video couldn\'t be stopped!');
                        }
                    );
                 });
 
-                btnHoldCall.addEventListener("click", function (e) {
+                btnHoldCall.addEventListener('click', function (e) {
                     var currentCall = speakEasy.CallManager.getCurrentCall();
                     currentCall.hold(
                         function() {
@@ -144,12 +144,12 @@ define(['jquery', 'CtlApiLoader'], function($, Ctl) {
                             $(btnUnHoldCall).show();
                         },
                         function() {
-                            showErrorMessage("Call couldn't be held!");
+                            showErrorMessage('Call couldn\'t be held!');
                         }
                     );
                 });
 
-                btnUnHoldCall.addEventListener("click", function (e) {
+                btnUnHoldCall.addEventListener('click', function (e) {
                     var currentCall = speakEasy.CallManager.getCurrentCall();
                     currentCall.unhold(
                         function() {
@@ -157,44 +157,44 @@ define(['jquery', 'CtlApiLoader'], function($, Ctl) {
                             $(btnHoldCall).show();
                         },
                         function() {
-                            showErrorMessage("Call couldn't be resumed!");
+                            showErrorMessage('Call couldn\'t be resumed!');
                         }
                     );
                 });
 
-                btnMute.addEventListener("click", function (e) {
+                btnMute.addEventListener('click', function (e) {
                     var currentCall = speakEasy.CallManager.getCurrentCall();
                     currentCall.mute();
-                    showInfoMessage("Call is muted!");
+                    showInfoMessage('Call is muted!');
                     $(btnMute).hide();
                     $(btnUnMute).show();
                 });
 
-                btnUnMute.addEventListener("click", function (e) {
+                btnUnMute.addEventListener('click', function (e) {
                     var currentCall = speakEasy.CallManager.getCurrentCall();
                     currentCall.unmute();
-                    showInfoMessage("Call is unmuted!");
+                    showInfoMessage('Call is unmuted!');
                     $(btnUnMute).hide();
                     $(btnMute).show();
                 });
 
-                btnLogout.addEventListener("click", function (e) {
+                btnLogout.addEventListener('click', function (e) {
                     $('#progressLogout').show();
                     speakEasy.logout(function() {
-                        showInfoMessage("SpeakEasy logout succeed!");
+                        showInfoMessage('SpeakEasy logout succeed!');
                         Ctl.logout();
                         $('#progressLogout').hide();
                         window.location.href = 'index.html';
                     },
                     function() {
-                        showErrorMessage("SpeakEasy logout has failed!");
+                        showErrorMessage('SpeakEasy logout has failed!');
                         Ctl.logout();
                         $('#progressLogout').hide();
                         window.location.href = 'index.html';
                     });
                 });
 
-                confDestination.addEventListener("keydown", function (e) {
+                confDestination.addEventListener('keydown', function (e) {
                     var keyCode = e.keyCode;
                     var currentCall = speakEasy.CallManager.getCurrentCall();
                     if (currentCall) {
@@ -223,18 +223,18 @@ define(['jquery', 'CtlApiLoader'], function($, Ctl) {
         $callRow.append(['<td class="js-status">', callInfo.status, '</td>'].join(''));
 
         callsTable.find('tbody').append($callRow);
-        $("#noCallsRow").hide();
+        $('#noCallsRow').hide();
 
         $callRow.click(function() {
             var callId = $(this).attr('data-id');
             _speakEasy.CallManager.switchTo(callId, function() {
                 updateCallButtonsGroup();
-                showInfoMessage("The call has switched successfully");
+                showInfoMessage('The call has switched successfully');
                 $('#currentCalls').find('tr').removeClass('active');
                 $(this).addClass('active');
             },
             function() {
-                showErrorMessage("The call switch are failed");
+                showErrorMessage('The call switch are failed');
             })
         });
     }
@@ -249,8 +249,8 @@ define(['jquery', 'CtlApiLoader'], function($, Ctl) {
         var $callRow = $('#currentCalls').find('tr[data-id="'+callId+'"]');
         $callRow.remove();
 
-        if(_speakEasy.CallManager.getCallsCount() == 0) {
-            $("#noCallsRow").show();
+        if(_speakEasy.CallManager.getCallsCount() === 0) {
+            $('#noCallsRow').show();
         }
     }
 
@@ -273,10 +273,10 @@ define(['jquery', 'CtlApiLoader'], function($, Ctl) {
             if(call.isVideoStarted) {
                 $(btnStartVideo).hide();
                 $(btnStopVideo).show();
-                $("#localVideoContainer").show();
+                $('#localVideoContainer').show();
             }
             else {
-                $("#localVideoContainer").hide();
+                $('#localVideoContainer').hide();
                 $(btnStopVideo).hide();
                 $(btnStartVideo).show();
             }
@@ -290,78 +290,80 @@ define(['jquery', 'CtlApiLoader'], function($, Ctl) {
                 $(btnHoldCall).show();
             }
 
-            $("#btnGroupCall").show();
+            $('#btnGroupCall').show();
         }
         else {
-            $("#btnGroupCall").hide();
+            $('#btnGroupCall').hide();
         }
     }
 
     function attachCallListeners(call) {
-        call.onStateChanged = function(state) {
-            var callId = call.getCallId();
-            switch (state) {
-                case call.events.CALL_RINGING:
-                    showInfoMessage("Ringing!");
-                    updateCallStatus(callId, 'Ringing');
-                    break;
-                case call.events.CALL_STARTED:
-                    showInfoMessage("Call is started!");
-                    updateCallStatus(callId, 'In Call');
-                    break;
-                case call.events.CALL_ENDED:
-                    showInfoMessage("Call was ended on other side!");
 
-                    updateCallStatus(callId, 'Ended');
-                    setTimeout(function() {
-                        removeCall(callId);
-                    }, 1000);
+        var callId = call.getCallId();
 
-                    updateCallButtonsGroup();
+        call.on('CALL_RINGING', function() {
+            showInfoMessage('Ringing!');
+            updateCallStatus(callId, 'Ringing');
+        });
 
-                    break;
-                case call.events.CALL_HELD:
-                    showInfoMessage("Call is held!");
-                    updateCallStatus(callId, 'Hold');
-                    break;
-                case call.events.CALL_REMOTE_HELD:
-                    showInfoMessage("Call was held on other side!");
-                    updateCallStatus(callId, 'Remote hold');
-                    updateCallButtonsGroup();
-                    break;
-                case call.events.CALL_REJECTED:
-                    showInfoMessage("Call was rejected!");
-                    updateCallStatus(callId, 'Rejected');
-                    updateCallButtonsGroup();
-                    setTimeout(function() {
-                        removeCall(callId);
-                    }, 1000);
+        call.on('CALL_STARTED', function() {
+            showInfoMessage('Call is started!');
+            updateCallStatus(callId, 'In Call');
+        });
 
-                    break;
-            }
-        }
+        call.on('CALL_ENDED', function() {
+            showInfoMessage('Call was ended on other side!');
+
+            updateCallStatus(callId, 'Ended');
+            setTimeout(function() {
+                removeCall(callId);
+            }, 1000);
+
+            updateCallButtonsGroup();
+        });
+
+        call.on('CALL_HELD', function() {
+            showInfoMessage('Call is held!');
+            updateCallStatus(callId, 'Hold');
+        });
+
+        call.on('CALL_REMOTE_HELD', function() {
+            showInfoMessage('Call was held on other side!');
+            updateCallStatus(callId, 'Remote hold');
+            updateCallButtonsGroup();
+        });
+
+        call.on('CALL_REJECTED', function() {
+            showInfoMessage('Call was rejected!');
+            updateCallStatus(callId, 'Rejected');
+            updateCallButtonsGroup();
+            setTimeout(function() {
+                removeCall(callId);
+            }, 1000);
+        });
+
     }
 
-    // Utility function to show error messages
+    // Utility functions to show error|info|success|warning messages
     function showErrorMessage(msg) {
-        showMessage(msg, "alert alert-danger");
+        showMessage(msg, 'alert alert-danger');
     }
-    // Utility function to show info messages
+
     function showInfoMessage(msg) {
-        showMessage(msg, "alert alert-info");
+        showMessage(msg, 'alert alert-info');
     }
-    // Utility function to show info success messages
+
     function showSuccessMessage(msg) {
-        showMessage(msg, "alert alert-success");
+        showMessage(msg, 'alert alert-success');
     }
-    // Utility function to show warning messages
+
     function showWarningMessage(msg) {
-        showMessage(msg, "alert alert-warning");
+        showMessage(msg, 'alert alert-warning');
     }
 
     function showMessage(msg, classNames) {
         var logContainer = document.getElementById('logContainer');
-        var elChild = document.createElement("div");
+        var elChild = document.createElement('div');
         elChild.className = classNames;
         elChild.innerHTML = msg;
         logContainer.insertBefore(elChild, logContainer.firstChild);
